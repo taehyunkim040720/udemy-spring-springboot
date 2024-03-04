6eec77fe7209db4c30b4a5e7a4b6d0fcc43906e8
김태현 <kimblank@gimtaehyeon-ui-MacBookPro.local>
2024-03-05 02:21:28 +0900

    feat : bean print

--- src/main/java/udemy/first/hello/App2GameJava.java
+++ src/main/java/udemy/first/hello/App2GameJava.java
@@ -2,19 +2,27 @@ package udemy.first.hello;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Arrays;
+
 public class App2GameJava {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
         System.out.println(applicationContext.getBean("name"));
         System.out.println(applicationContext.getBean("age"));
-//        System.out.println(applicationContext.getBean("person"));
         System.out.println(applicationContext.getBean("address"));
         System.out.println(applicationContext.getBean("person1"));
-        System.out.println(applicationContext.getBean("person2"));
         System.out.println(applicationContext.getBean("addressName"));
-        System.out.println(applicationContext.getBean(Address.class));
-        System.out.println(applicationContext.getBean(Person.class));
-        System.out.println(applicationContext.getBean("person4") + "d");
-        System.out.println(applicationContext.getBean("person5"));
+
+
+        String[] beanNames = applicationContext.getBeanDefinitionNames();
+
+
+        Arrays.sort(beanNames);
+
+
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
+
     }
 }
